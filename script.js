cb73df63d572543efe2edd7a054d95c21bb7d57b
CyanMatter <b.j.vandervaart@student.tudelft.nl>
2018-12-03 14:45:32 +0100

    Created JavaScript function copyToClipboard

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function copyToClipboard() {
     /* Copy the text inside the text field */
     document.execCommand("copy");
 
-    /* Alert the copied text */
+    /* Alert the 
+     text */
     alert("Copied the text: " + copyText.value);
 }
