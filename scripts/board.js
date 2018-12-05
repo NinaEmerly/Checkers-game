ae0f246ce8cc6e0662d059005527e9fc08fe559c
CyanMatter <vandervaart.bart@gmail.com>
2018-12-05 16:09:19 +0100

    Halved size of checkerboard array by removing empty elements

--- scripts/board.js
+++ scripts/board.js
@@ -1,35 +1,55 @@
 var main = function() {
     "use strict";
 
+    // Global variables
+    var hor = 8;    // Number of rows
+    var ver = 4;    // Number of columns/2 (this initializes a 8x8 board)
+
     /*
     * validSpace checks if a given position is on a 8x8 checkerboard
     * Parameter row: integer horizontal position
     * Parameter col: integer vertical position
     * Returns: boolean. true if position is a valid space, else false
     */
-    function validSpace(row, col) {
-        if(row < 0 || row > 7 || col < 0 || col > 7) {
+    function validSpace(row, board_col) {
+        if(row < 0 || row >= hor || board_col < 0 || board_col >= ver) {
             return false;
         } else return true;
     }
 
     /*
     * getNeighbor checks an element in the board array at given row and column
-    * if the element already contains a Space object, return a reference to that object
-    * if the element is still empty, create and return a new Space object
-    * if the 
+    * Parameter row: integer horizontal position on board
+    * Parameter col: integer vertical position on board
+    * Parameter board: Space array of the checkerboard
+    * Returns:
+    * If the row and col designate a position off the board, return null
+    * If the element already contains a Space object, return a reference to that object
+    * If the element is still empty, create and return a new Space object
     */
     function getNeighbor(row, col, board) {
-        if (!validSpace(row, col)) {
+        console.log(row, col)
+        var board_col = Math.floor(col/2);
+        if (!validSpace(row, board_col)) {
             return null;
-        } else if (board[row][col] instanceof Space) {
-            return board[row][col];
+        } else if (board[row][board_col] instanceof Space) {
+            return board[row][board_col];
         } else return new Space(row, col, board);
     }
 
+    /*
+    * Constructor of Space object
+    * Constructing a single space somewhere on the checkerboard will also
+    * create and network all other spaces on the board
+    * Parameter row: integer horizontal position on board
+    * Parameter col: integer vertical position on board
+    * Parameter board: Space array of the checkerboard
+    */
     function Space(row, col, board) {
+        board_col = Math.floor(col/2);
+        board[row][board_col] = this;       // Register this Space on board
         this.row = row;                     // integer horizontal position on board
-        this.col = col;                     // integer vertical position on board
+        this.col = board_col;               // integer vertical position on board
         this.occupiedOwn = false;           // boolean, space is occupied by own piece
         this.occupiedOpp = false;           // boolean, space is occupied by opponent's piece
         this.available = true;              // boolean, space is unoccupied
@@ -38,22 +58,10 @@ var main = function() {
         var ur_col = col + 1;
         var ll_row = row - 1;
         var ll_col = col - 1;
-        var ur_Space, ul_Space, ll_Space, lr_Space;
-        if (board[ur_row][ur_col] instanceof Space) {
-            ur_Space = board[ur_row][ur_col];
-        } else if (validSpace(ur_row, ur_col)) {
-            ur_Space = new Space(ur_row, ur_col);
-        } else ur_Space = null;
-
-        if (validSpace(ur_row, ll_col)) {
-            ul_Space = new Space(ur_row, ll_col);
-        } else ul_Space = null;
-        if (validSpace(ll_row, ll_col)) {
-            ll_Space = new Space(ll_row, ll_col);
-        } else ll_Space = null;
-        if (validSpace(ll_row, ur_col)) {
-            lr_Space = new Space(ll_row, ur_col);
-        } else lr_Space = null;
+        var ur_Space = getNeighbor(ur_row, ur_col, board);
+        var ul_Space = getNeighbor(ur_row, ll_col, board);
+        var ll_Space = getNeighbor(ll_row, ll_col, board);
+        var lr_Space = getNeighbor(ll_row, ur_col, board);
         this.neighbors = new Array(ur_Space, ul_Space, ll_Space, lr_Space);      // array of references to adjacent spaces [ur, ul, ll, lr]
     }
 
@@ -82,36 +90,16 @@ var main = function() {
         this.occupiedOpp = !this.occupiedOpp;
     };
 
-    Space.prototype.genSpaces = function(hor, ver) {
-
-    };
-
     /* Construct the initial checkerboard */
     function Game() {
-        // Initialize an empty board 8x8
+        // Initialize an empty board hor x ver (8x4)
         this.board = new Array(0);
-        for (var i=0; i<8; i++) {
-            this.board.push(new Array(8));
+        for (var i=0; i<hor; i++) {
+            this.board.push(new Array(ver));
         }
-        // Populate board with spaces and pieces
-        // Rows 0-2: own pieces
-        for (var row=0; row<3; row++) {
-            for (var col=0; col<4; col++) {
-                this.board[row][col] = new Space(row, col, true, false);
-            }
-        }
-        // Rows 3-4: unoccupied
-        for (row; row<5; row++) {
-            for (col=0; col<4; col++) {
-                this.board[row][col] = new Space(row, col, false, false);
-            }
-        }
-        // Rows 5-7: opponent's pieces
-        for (row; row<8; row++) {
-            for (col=0; col<4; col++) {
-                this.board[row][col] = new Space(row, col, false, true);
-            }
-        }
+
+        // Generate Space objects and populate the board with them
+        new Space(0, 0, this.board);
     };
 
     //TODO: rewrite validMoves to work with Space objects
