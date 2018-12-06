98969fd1e53c98ba04aa213992d0f2f922406132
CyanMatter <vandervaart.bart@gmail.com>
2018-12-06 01:25:07 +0100

    Gamepieces can return an array of spaces they can move in with validMoves

--- scripts/pieceKing.js
+++ scripts/pieceKing.js
@@ -1,32 +1,46 @@
 /*
-*   Constructs a pieceKing: gamepiece which is a king
-*   pieceKing inherits from pieceMan
+*   Constructs a PieceKing: gamepiece which is a king
+*   PieceKing inherits from PieceMan
 */
-function pieceKing(own, space) {
-    pieceMan.call(this, own, space);
+function PieceKing(own, space) {
+    PieceMan.call(this, own, space);
 }
 
 /*  redirect prototype */
-pieceKing.prototype = Object.create(pieceMan.prototype);
-pieceKing.prototype.constructor = pieceKing;
-
-pieceKing.prototype.diagonalValidMoves = function(i) {
-
-}
+PieceKing.prototype = Object.create(PieceMan.prototype);
+PieceKing.prototype.constructor = PieceKing;
 
 /*  @Overwrite
 *   validMoves checks if the spaces in a diagonal line from this gamepiece are unoccupied
 *   and looks for possible opponent's pieces that can be taken
 *   Returns: an array of valid destinations for this gamepiece to move in
 */
-pieceKing.prototype.validMoves = function() {
+PieceKing.prototype.validMoves = function() {
     // Initialize variables
     var neighbor;
     var destinations = new Array(0);
 
-    // Iterate over neighbors
-    for (var i=0; this.neighbors.length; i++) {
-        
+    // Iterate over all directions
+    for (var i=0; i<=4; i++) {
+        neighbor = this.space.neighbors[i];                             // 'neighbor' is the closest space in that direction
+
+        // Add all unoccupied spaces in that direction to valid destinations
+        while (neighbor instanceof Space && neighbor.getAvailable()) {  // While neighbor is a space on the board and unoccupied
+            destinations.push(neighbor)                                 // Add this neighbor to valid destinations
+            neighbor = neighbor.getNeighbors()[i];                      // Go to next neighbor in the same direction
+        }
+        // The loop encountered an obstruction 
+        // Case 1: neighbor is occupied by own piece or not on the board
+        if (!(neighbor instanceof Space) || neighbor.getOccupiedOwn()) {
+            continue;                                                   // Do nothing: neighbor is not a valid destination
+        }
+        // Case 2: neighbor is occupied by opponent's piece
+        if (neighbor.getOccupiedOpp()) {
+            neighbor = neighbor.getNeighbors()[i];
+            if (neighbor instanceof Space && neighbor.getAvailable()) { // If neighbor is unoccupied and on the board
+                destinations.push(neighbor)                             // Add the conjunt neighbor to valid destinations
+            } else continue;                                            // Else do nothing: neighbor is not a valid destination
+        }
     }
     return destinations;
 }
--- scripts/pieceMan.js
+++ scripts/pieceMan.js
@@ -1,48 +1,60 @@
 /*
-*   Constructs a pieceMan object: gamepiece which is a man
+*   Constructs a PieceMan object: gamepiece which is a man
 *   Prototype of this object is inherited by gamepiece king
 *   Parameter own: boolean, true if owned, else if opponent's
 */
-function pieceMan(own, space) {
+function PieceMan(own, space) {
     this.own = own;
     this.space = space;
 }
 
-/* pieceMan getters */
-pieceMan.prototype.getOwn = function() { return this.own; };
-pieceMan.prototype.getSpace = function() { return this.space; };
+/* PieceMan getters */
+PieceMan.prototype.getOwn = function() { return this.own; };
+PieceMan.prototype.getSpace = function() { return this.space; };
 
-/* pieceMan setters */
-pieceMan.prototype.setOwn = function(own) { this.own = own; };
-pieceMan.prototype.setSpace = function(Space) { this.space = space; };
+/* PieceMan setters */
+PieceMan.prototype.setOwn = function(own) { this.own = own; };
+PieceMan.prototype.setSpace = function(Space) { this.space = space; };
 
 /*
 *   validMoves checks if the neighboring spaces of this gamepiece are unoccupied
 *   and looks for possible opponent's pieces that can be taken
 *   Returns: an array of valid destinations for this gamepiece to move in
 */
-pieceMan.prototype.validMoves = function() {
+PieceMan.prototype.validMoves = function() {
     // Initialize variables
-    var neighbor;
+    var neighbor, i;
     var destinations = new Array(0);
 
-    // Iterate over neighbors
-    for (var i=0; this.neighbors.length; i++) {
-        neighbor = this.neighbors[i];
-        // Case 1: neighbor is unoccupied
-        if (neighbor.getAvailable()) {
-            destinations.push(neighbor);                        // Append this neighbor to valid destinations
-            continue;                                           // Go to next neighbor
+    // Iterate over forward neighbors
+    for (i=0; i<=2; i++) {
+        neighbor = this.space.neighbors[i];
+
+        // Case 1: neighbor is occupied by own piece or not on board
+        if (!(neighbor instanceof Space) || neighbor.getOccupiedOwn()) {
+            continue;                                                   // Do nothing: neighbor is not a valid destination
         }
-        // Case 2: neighbor is occupied by own piece
-        if (neighbor.getOccupiedOwn()) {
-            continue;                                           // Do nothing: neighbor is not a valid destination
+
+        // Case 2: neighbor is unoccupied
+        if (neighbor.getAvailable()) {
+            destinations.push(neighbor);                                // Add this neighbor to valid destinations
+            continue;                                                   // Go to next neighbor
+        }
+    }
+    // Iterate over all neighbors
+    for (i=0; i<=4; i++) {
+        neighbor = this.space.neighbors[i];
+
+        // Case 1: neighbor is not on the board
+        if (!(neighbor instanceof Space)) {
+            continue;                                                   // Do nothing: neighbor is not a valid destination
         }
         // Case 3: neighbor is occupied by opponent's piece
         if (neighbor.getOccupiedOpp()) {
-            if (neighbor.getNeighbors()[i].available) {         // Check if conjunct space is unoccupied
-                destinations.push(neighbor.getNeighbors()[i]);  // Append the conjunct neighbor to valid destinations
-            } else continue;                                    // Else do nothing: neighbor is not a valid destination
+            neighbor = neighbor.getNeighbors()[i];                        // Go to next conjunct space   
+            if (neighbor instanceof Space && neighbor.getAvailable()) { // If conjunct space is unoccupied and on the board
+                destinations.push(neighbor)                             // Add the conjunt neighbor to valid destinations
+            }                                                           // Else do nothing: neighbor is not a valid destination
         }
     }
     return destinations;
