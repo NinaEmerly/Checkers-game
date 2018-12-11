e1c2aa8b87f92737357bd667370530d911583d42
CyanMatter <vandervaart.bart@gmail.com>
2018-12-11 22:09:11 +0100

    Created server script to track statistics. Copy/pasted relevant parts from demo

---
+++ statTracker.js
@@ -0,0 +1,13 @@
+/* 
+ *  In-memory game statistics "tracker".
+ *  As future work, this object should be replaced by a DB backend.
+ */
+
+var gameStatus = {
+    since           : Date.now(),   /* since we keep it simple and in-memory, keep track of when this object was created */
+    onlinePlayers   : 0,            /* number of players currently connected */
+    ongoingGames    : 0,            /* number of games currently active */
+    enqueuedPlayers : 0             /* number of connected players waiting to begin a game */
+};
+
+module.exports = gameStatus;        /* Make this file available as a module to other files */
