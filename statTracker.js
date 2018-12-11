9df059442bec213aaa5d77259f72e94151dc4f81
CyanMatter <vandervaart.bart@gmail.com>
2018-12-11 22:55:14 +0100

    Created statistics tracker

--- statTracker.js
+++ statTracker.js
@@ -4,10 +4,11 @@
  */
 
 var gameStatus = {
-    since           : Date.now(),   /* since we keep it simple and in-memory, keep track of when this object was created */
-    onlinePlayers   : 0,            /* number of players currently connected */
-    ongoingGames    : 0,            /* number of games currently active */
-    enqueuedPlayers : 0             /* number of connected players waiting to begin a game */
+    since               : Date.now(),   /* since we keep it simple and in-memory, keep track of when this object was created */
+    gamesInitialized    : 0,            /* number of games that have been initialized */
+    onlinePlayers       : 0,            /* number of players currently connected */
+    ongoingGames        : 0,            /* number of games currently active */
+    enqueuedPlayers     : 0             /* number of connected players waiting to begin a game */
 };
 
-module.exports = gameStatus;        /* Make this file available as a module to other files */
+module.exports = gameStatus;            /* Make this file available as a module to other files */
