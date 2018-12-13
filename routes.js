329910f799349d92db69074a73f48815e62a73cc
NinaEmerly <nina.e.the@hotmail.com>
2018-12-13 13:57:34 +0100

    wef

--- routes.js
+++ routes.js
@@ -2,11 +2,11 @@ module.exports = function(app) {
 
     /* Route 1: /splash */
     app.get('/(splash)?', function(req, res) {
-        res.sendFile("splash.html", {root: "./static"});
+        res.sendFile("splash.html", {root: "./static/html"});
     })
 
     /* Route 2: /game */
     app.get('/game', function(req, res) {
-        res.sendFile("game.html", {root: "./static"});
+        res.sendFile("game.html", {root: "./static/html"});
     })
 }
